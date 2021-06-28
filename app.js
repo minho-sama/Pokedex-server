99a51516abd9807d44239bd2f9f36e69af836a1c
minho-sama <n.a.minh1106@gmail.com>
2021-06-28 21:01:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const mongoose = require('mongoose');
 const DBkey = "mongodb+srv://minh:735196Lili@inventory.5urxc.mongodb.net/inventory?retryWrites=true&w=majority"
 const mongoDB = process.env.MONGODB_URI || DBkey;
 // process.env.DB_KEY
+//https://pokedex-api-minho.herokuapp.com/
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -21,7 +22,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
