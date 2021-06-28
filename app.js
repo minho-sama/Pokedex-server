df14b6cf896d65618c155e2c53c114f280efdf74
minho-sama <n.a.minh1106@gmail.com>
2021-06-28 18:12:38 +0200

    cors middleware

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv/config')
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 const pokedexRouter = require('./routes/pokedex');
 
@@ -20,6 +20,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
